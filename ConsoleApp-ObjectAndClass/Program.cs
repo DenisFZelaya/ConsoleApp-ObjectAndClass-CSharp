b9b95687ac9f9bd12727e40e8414f81d6ce12215
DenisFZelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-01-13 13:38:13 -0600

    Initial commit

---
+++ ConsoleApp-ObjectAndClass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp_ObjectAndClass
+{
+    class Program
+    {
+        int id;
+        String name;
+
+        public void insert(int i, String n)
+        {
+            id = i;
+            name = n;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Edad: " + id + " Nombre: " + name);
+        }
+
+        static void Main(string[] args)
+        {
+            Program s1 = new Program();
+
+            s1.id = 27;
+            s1.name = "Deni Zelaya";
+
+            Console.WriteLine(s1.id);
+            Console.WriteLine(s1.name);
+
+            Program s2 = new Program();
+            s2.insert(27, "Denis Federico");
+            s2.display();
+        }
+    }
+}
